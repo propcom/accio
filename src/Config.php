8858e7c6defc0c06f74e5c86f0ac98255301c777
Andrew Wheal <andreww@propcom.co.uk>
2018-04-12 17:58:24 +0100

    Add json config file parsing to allow easy injection of directories and bootstrap

---
+++ src/Config.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types=1);
+
+namespace Propcom\Accio;
+
+class Config
+{
+
+	private	$validation_rules = [
+		'directories' => [
+			'filter' => FILTER_DEFAULT,
+			'flags' => FILTER_REQUIRE_ARRAY,
+		],
+		'app_bootstrap' => [
+			'filter' => FILTER_DEFAULT,
+			'flags' => FILTER_REQUIRE_SCALAR,
+		],
+	];
+
+	private $data;
+
+	public function __construct(string $config_filepath)
+	{
+		try {
+			$this->data = $this->load($config_filepath);
+		} catch (\Exception $e) {
+			echo $e->getMessage();
+			die(1);
+		}
+	}
+
+	public function load($filepath)
+	{
+		if (!is_file($filepath)) {
+			throw new \RuntimeException('Can\'t find config file');
+		}
+
+		$data = file_get_contents($filepath);
+		if ($data === false) {
+			throw new \RuntimeException('Can\'t load config file');
+		}
+
+		$data = json_decode($data, true);
+		if ($data === null) {
+			throw new \RuntimeException('Can\'t parse config file');
+		}
+
+		return $this->validate($data);
+	}
+
+	public function validate($data)
+	{
+		if (!array_key_exists('directories', $data)) {
+			throw new \RuntimeException('Accio config must include the "directories" key');
+		}
+
+		if (!is_array($data['directories'])) {
+			throw new \RuntimeException('Accio config key "directories" must be an array');
+		}
+
+		if (array_key_exists('app_bootstrap', $data) and !is_file($data['app_bootstrap'])) {
+			throw new \RuntimeException('Accio config key "app_bootstrap" must be a path to a file');
+		}
+
+		return array_intersect_key($data, ['directories' => null, 'app_bootstrap' => null]);
+	}
+
+	public function bootstrap(): void
+	{
+		if ($this->data['app_bootstrap']) {
+			require_once $this->data['app_bootstrap'];
+		}
+	}
+
+	public function getDirectories(): array
+	{
+		return $this->data['directories'];
+	}
+}
